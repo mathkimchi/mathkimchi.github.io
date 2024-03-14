2d10336428b7fad4183213e24fbba8f4bf7e3f63
A. Kim <kingalvinjkim@gmail.com>
2024-03-13 23:06:21 -0400

    build rs test

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,14 @@
-fn main() {}
+use app::App;
+use yew::Renderer;
+
+mod app;
+
+// fn main() {
+//     Renderer::<App>::new().render();
+// }
+
+include!(concat!(env!("OUT_DIR"), "/content.rs"));
+
+fn main() {
+    println!("String: {FILE_CONTENTS}");
+}
